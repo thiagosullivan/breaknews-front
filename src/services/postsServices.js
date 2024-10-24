aedba44ac35ee4e8ff9370d13faea7470961eb2d
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-24 19:48:14 -0300

    feat: getting the Top Post from API

--- src/services/postsServices.js
+++ src/services/postsServices.js
@@ -4,6 +4,10 @@ const baseURL = "http://localhost:3000";
 
 export function getAllPosts(){
     const response = axios.get(`${baseURL}/news`)
+    return response;
+}
 
+export function getTopPost(){
+    const response = axios.get(`${baseURL}/news/top`)
     return response;
 }
