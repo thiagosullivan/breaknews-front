39d9050f801edb6f1606a8a4764a33931a3b5c0f
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-24 19:48:44 -0300

    feat: populating infos to Card component on home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,49 @@
 import { useEffect, useState } from "react";
 import { Card } from "../../components/Card/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
-// import { news } from "../../Data.js";
-import { getAllPosts } from "../../services/postsServices.js";
-import { HomeBody } from "./HomeStyled.jsx";
+import { getAllPosts, getTopPost } from "../../services/postsServices.js";
+import { HomeBody, HomeHeader } from "./HomeStyled.jsx";
 
 export default function Home(){
     
-    const [ news, setNews ] = useState([]);
+    const [ posts, setPosts ] = useState([]);
+    const [ topPost, setTopPost ] = useState([]);
 
-    async function findAllPosts(){
-        const response = await getAllPosts();
-        setNews(response.data.results);
+    async function findPost(){
+        const postsResponse = await getAllPosts();
+        setPosts(postsResponse.data.results);
+
+        const topPostResponse = await getTopPost();
+        setTopPost(topPostResponse.data.news)
     }
 
     useEffect(() => {
-        findAllPosts()
+        findPost()
     }, [])
 
     return (
         <>
             <Navbar />
+            <HomeHeader>
+                <Card
+                    top={true}
+                    title={topPost.title}
+                    text={topPost.text}
+                    banner={topPost.banner}
+                    likes={topPost.likes}
+                    comments={topPost.comments}
+                />
+            </HomeHeader>
             <HomeBody>
-                {news.map((item) => {
+                {posts.map((item) => {
                     return (
                         <Card
                             key={item.id}
                             title={item.title}
                             text={item.text}
                             banner={item.banner}
-                            likes={item.likes.length}
-                            comments={item.comments.length}
+                            likes={item.likes}
+                            comments={item.comments}
                         />
                     )
                 })}
--- src/pages/Home/HomeStyled.jsx
+++ src/pages/Home/HomeStyled.jsx
@@ -9,3 +9,9 @@ export const HomeBody = styled.section`
     width: 80%;
 
 `
+
+export const HomeHeader = styled.section`
+    width: 80%;
+    display: flex;
+    margin: 1rem auto;
+`
