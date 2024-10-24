cd629a48c50303f998ae73ded8c200c91bd701cc
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-23 21:32:49 -0300

    feat: added a component to limit the length of text on homepage

---
+++ src/components/TextLimit/TextLimit.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+export function TextLimit({text, limit}){
+    const textLimited = text.length > limit ? `${text.substring(0, limit)}...` : text;
+
+    return <p>{textLimited}</p>
+}
