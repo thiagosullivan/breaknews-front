f2c0b55822d579c851f5df6ca5d9a01e4dce4e31
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-26 00:14:34 -0300

    feat: added errors validation and errors messages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,60 @@
-import { Link, Outlet, useNavigate } from 'react-router-dom';
-import logo from '../../images/logoBN.png';
-// import "./Navbar.css";
-import { Button, ImageLogo, InputSpace, Nav } from './NavbarStyled';
-import { useForm } from 'react-hook-form';
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import logo from "../../images/logoBN.png";
+import { Button, ErrorSpan, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export function Navbar(){
-    const { register, handleSubmit, reset } = useForm();
-    const navigate = useNavigate();
+const searchSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "A pesquisa não pode ser vazia." })
+    .refine((value) => !/^\s*$/.test(value), {
+      message: "A pesquisa não pode ter apenas espaço.",
+    }),
+});
 
-    function onSearch(data){
-        const { title } = data;
-        navigate(`/search/${title}`);
-        reset();
-    }
+export function Navbar() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(searchSchema),
+  });
+  const navigate = useNavigate();
 
+  function onSearch(data) {
+    const { title } = data;
+    navigate(`/search/${title}`);
+    reset();
+  }
 
-    return (
-        <>
-            <Nav>
-                <form onSubmit={handleSubmit(onSearch)}>
-                    <InputSpace>
-                        <button type='submit'>
-                            <i className="bi bi-search"></i>
-                        </button>
-                        <input
-                            type='text'
-                            placeholder='Pesquise por um título'
-                            {...register("title")}
-                        />
-                    </InputSpace>
-                </form>
+  return (
+    <>
+      <Nav>
+        <form onSubmit={handleSubmit(onSearch)}>
+          <InputSpace>
+            <button type="submit">
+              <i className="bi bi-search"></i>
+            </button>
+            <input
+              type="text"
+              placeholder="Pesquise por um título"
+              {...register("title")}
+            />
+          </InputSpace>
+        </form>
 
-                <Link to="/">
-                    <ImageLogo src={logo} alt="Logo Breaking News" />
-                </Link>
-                
-                <Button>Entrar</Button>
-            </Nav>
-            <Outlet />
-        </>
-    )
+        <Link to="/">
+          <ImageLogo src={logo} alt="Logo Breaking News" />
+        </Link>
+
+        <Button>Entrar</Button>
+      </Nav>
+      {errors.title && <ErrorSpan>{errors.title.message}</ErrorSpan>}
+      <Outlet />
+    </>
+  );
 }
-
