a035fc2a70c4befb88b1b138e7fac35fcd2ce176
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-26 02:22:16 -0300

    feat: displaying results from search

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,11 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { searchPosts } from "../../services/postsServices";
+import { ContainerResults, SearchPosts, TextResults } from "./SearchStyled";
+import { Card } from "../../components/Card/Card";
 
-export function Search(){
-    const { title } = useParams();
+export function Search() {
+  const { title } = useParams();
+  const [posts, setPosts] = useState([]);
 
-    return (
-        <div>
-            <h1>{title}</h1>
-        </div>
-    )
+  async function search() {
+    try {
+      const postsApi = await searchPosts(title);
+      setPosts(postsApi.data.results);
+    } catch (error) {
+      console.log(error);
+      setPosts([]);
+    }
+  }
+
+  console.log(posts);
+
+  useEffect(() => {
+    search();
+  }, [title]);
+
+  return (
+    <ContainerResults>
+      <TextResults>
+        <span>
+          {posts.length
+            ? `Encontramos ${posts.length} ${
+                posts.length > 1 ? "resultados" : "resultado"
+              } para:`
+            : "Não encontramos resultados para:"}
+        </span>
+        <h2>{title}</h2>
+      </TextResults>
+      <SearchPosts>
+        {posts.map((item) => {
+          return (
+            <Card
+              key={item.id}
+              title={item.title}
+              text={item.text}
+              banner={item.banner}
+              likes={item.likes}
+              comments={item.comments}
+            />
+          );
+        })}
+      </SearchPosts>
+    </ContainerResults>
+  );
 }
