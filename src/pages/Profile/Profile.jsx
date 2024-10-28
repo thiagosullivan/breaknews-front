2c386a097b870c5da77e5b27d9c30da0e1834de6
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-28 05:13:07 -0300

    feat: added new header components to Profile Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,31 @@
+import { useContext } from "react"
+import { UserContext } from "../../Context/UserContext"
+import { ProfileActions, ProfileAvatar, ProfileBackground, ProfileContainer, ProfileHeader, ProfileIconAdd, ProfileIconEdit, ProfileUser } from "./ProfileStyled";
+
 export function Profile(){
+    const {user} = useContext(UserContext);
+
     return (
-        <h1>Profile Page</h1>
+        <ProfileContainer>
+            <ProfileHeader>
+                <ProfileIconEdit>
+                    <i className="bi bi-pencil-square"></i>
+                </ProfileIconEdit>
+
+                <ProfileBackground src={user.background} alt=""/>
+
+                <ProfileUser>
+                    <ProfileAvatar src={user.avatar} alt=""/>
+                    <h2>{user.name}</h2>
+                    <h3>@{user.username}</h3>
+                </ProfileUser>
+
+                <ProfileActions>
+                    <ProfileIconAdd>
+                        <i className="bi bi-plus-circle"></i>
+                    </ProfileIconAdd>
+                </ProfileActions>
+            </ProfileHeader>
+        </ProfileContainer>
     )
 }
